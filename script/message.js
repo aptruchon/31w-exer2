2b8735d9ede66a7f0ee5bf2ee7d0061845e65e14
aptruchon <aptruchon@gmail.com>
2022-06-20 14:25:20 -0400

    ajout de texte dans la boite modale

--- script/message.js
+++ script/message.js
@@ -1,23 +1,30 @@
 (function(){
     let elmBoite__modale
     let elmBoite__x
-    
+    let elmBoite__contenu
+
+    let elmOK = document.querySelector('#mon_bouton');
+    let elmBody = document.querySelector("body");
+
     function creer_boite_modale(){
         elmBoite__modale = document.createElement("div");
+        elmBoite__contenu = document.createElement("div");
         elmBoite__x = document.createElement("button");
-        elmBoite__modale.appendChild(elmBoite__x);
-        elmBoite__x.classList.add("boite__x");
-        elmBoite__x.innerHTML = "X";
+
         elmBoite__modale.classList.add("boite__modale");
-        let elmBody = document.querySelector("body");
+        elmBoite__contenu.classList.add("elmBoite__contenu");
+        elmBoite__x.classList.add("boite__x");
+
         elmBody.appendChild(elmBoite__modale);
+        elmBoite__modale.appendChild(elmBoite__contenu);
+        elmBoite__modale.appendChild(elmBoite__x);
+
+        elmBoite__x.innerHTML = "X";
+        elmBoite__contenu.innerHTML = "Ceci est un test";
     }
     
     creer_boite_modale();
     console.log(elmBoite__modale);
-    
-
-    let elmOK = document.querySelector('#mon_bouton');
 
     elmOK.addEventListener('mousedown', function(){
         console.log('Bravo, le script fonctionne!');
